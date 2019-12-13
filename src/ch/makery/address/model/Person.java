9c99fa20890d572907acaac5815f457a069cff52
heikov123 <heiko.valli@khk.ee>
2019-12-14 01:27:22 +0200

    Part 5: Storing Data as XML

--- src/ch/makery/address/model/Person.java
+++ src/ch/makery/address/model/Person.java
@@ -1,13 +1,17 @@
 package ch.makery.address.model;
 
 import java.time.LocalDate;
-
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import ch.makery.address.util.LocalDateAdapter;
+
 
 /**
  * Model class for a Person.
@@ -107,6 +111,7 @@ public class Person {
         return city;
     }
 
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
